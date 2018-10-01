558d9cd4ef02cedcbdea7c9e4908c04507865a93
altynbek <altynbek.pirman@nu.edu.kz>
2018-10-02 01:04:10 +0600

    changed db to postgres

--- backend/src/soAktau/settings.py
+++ backend/src/soAktau/settings.py
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'soAktau.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'soAktau',
+        'USER': 'belisar',
+        'PASSWORD': '#8<7X{39cdyf!}QA',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
