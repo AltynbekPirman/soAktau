2faece53d37b8f3ede9f754109c4658f1d119dd4
altynbek <altynbek.pirman@nu.edu.kz>
2018-10-03 04:19:47 +0600

    small fix

--- backend/src/services/admin.py
+++ backend/src/services/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.db import models
 from django.forms import TextInput, Textarea
-from django_summernote.admin import SummernoteModelAdmin, SummernoteInlineModelAdmin
 from django_summernote.widgets import SummernoteWidget
 
 from services.models import Post, SubService, Service, Title
@@ -9,20 +8,14 @@ from services.models import Post, SubService, Service, Title
 admin.site.register((Title, SubService))
 
 
-# class SubServiceInline(admin.StackedInline):
-#     model = SubService
-#     extra = 0
-
-
-# class TitleInline(admin.StackedInline):
-#     model = Title
-#     extra = 0
+class SummernoteInlineModelAdmin(admin.options.InlineModelAdmin):
+    formfield_overrides = {models.TextField: {'widget': SummernoteWidget}}
 
 
 class PostInline(admin.StackedInline, SummernoteInlineModelAdmin):
     model = Post
-    extra = 0
     summernote_fields = ('text',)
+    extra = 1
 
 
 class ServiceAdmin(admin.ModelAdmin):
